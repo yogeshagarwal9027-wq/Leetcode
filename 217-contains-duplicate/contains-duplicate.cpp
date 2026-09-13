21c5df4abf016374459ef87d68f01a3cfe08353f
Yogesh Agarwal <yogeshagarwal9027@gmail.com>
2026-09-13 15:23:21 +0530

    Time: 34 ms (79.66%) | Memory: 82.6 MB (99.63%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        bool ans=false;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                ans= true;
+            }
+
+        }
+        return ans;
+    }
+};
